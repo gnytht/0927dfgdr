e2bc5af615e8cc92427102770734221ecd76b5a8
gnytht <hooldrf05@outlook.com>
2025-10-06 20:32:55 -0800

    Update keep.sh

--- keep.sh
+++ keep.sh
@@ -5,7 +5,7 @@ EMAIL="xejop66974@gddcorp.com"   # 登录邮箱
 PASSWORD="wEbM79K6BkOgQSCn4fXZ"     # 登录密码
 
 # 要保活的URL列表,多个用英文空格分隔
-URLS="https://free-sgegvjqx.cfapps.ap21.hana.ondemand.com https://free-uscriuao.cfapps.us10-001.hana.ondemand.com/"  
+URLS="https://free-sgegvjqx.cfapps.ap21.hana.ondemand.com https://free-uscriuao.cfapps.us10-001.hana.ondemand.com"  
 
 # 颜色定义
 green() {
